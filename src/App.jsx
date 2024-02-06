5ca8e34212eb3254645d426845ec65bdef67a668
João Paulo <joaopaulomayka@gmail.com>
2024-02-06 18:01:54 -0300

    feat: completeTodo function

--- src/App.jsx
+++ src/App.jsx
@@ -47,12 +47,19 @@ function App() {
     setTodos(filteredTodos)
   }
 
+  const completeTodo = (id) => {
+    const newsTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo);
+
+    setTodos(newsTodos)
+  }
+
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} />
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
       <TodoForm addTodo={addTodo} />

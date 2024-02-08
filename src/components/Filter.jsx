deba0791ad9bb35636c79c3781250da6c07898ff
João Paulo <joaopaulomayka@gmail.com>
2024-02-08 17:55:25 -0300

    feat: filtering tasks according to status

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-const Filter = () => {
+const Filter = ({ filter, setFilter }) => {
   return (
     <div className="filter">
       <h2>Filtrar: </h2>
       <div className="filter-options">
         <div>
           <p>Status</p>
-          <select>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
             <option value="All">Todas</option>
             <option value="Completed">Completas</option>
             <option value="Incomplete">Incompletas</option>
           </select>
         </div>
         <div className="order-buttons">
-            <p>Ordem alfabética: </p>
-            <button className="asc">Asc</button>
-            <button className="remove">Desc</button>
+          <p>Ordem alfabética: </p>
+          <button className="ascd">Asc</button>
+          <button className="remove">Desc</button>
         </div>
       </div>
     </div>
